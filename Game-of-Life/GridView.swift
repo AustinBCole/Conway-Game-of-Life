c1b4658c715bed52bcf89a1b94ccffdfba513622
AustinBCole <austin.cole.chileno@gmail.com>
2019-07-17 16:26:12 -0600

    Add grid pattern to gridView

--- Game-of-Life/GridView.swift
+++ Game-of-Life/GridView.swift
@@ -22,7 +22,8 @@ class GridView: UIView {
                 //Multiply x by 10 and y by 10
                 let cell = GridCell(frame: CGRect(x: x * 10, y: y * 10, width: 10, height: 10), index: (x, y))
     
-                
+                cell.layer.borderColor = UIColor.gray.cgColor
+                cell.layer.borderWidth = 0.5
                 self.addSubview(cell)
                 gridCellGraph.addCellToGraph(cell: cell)
             }
